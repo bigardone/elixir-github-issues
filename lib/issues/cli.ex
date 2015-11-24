64ae0a8d1c0ad0b04553887bea948d2e663206c1
Ricardo <bigardone@gmail.com>
2015-11-24 12:53:58 +0100

    Call Issues module from cli

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -7,7 +7,9 @@ defmodule Issues.CLI do
   ​table of the last _n_ issues in a github project​
   """
   def run(argv) do
-    parse_args(argv)
+    argv
+      |> parse_args
+      |> process
   end
 
   @doc """
@@ -27,11 +29,23 @@ defmodule Issues.CLI do
       { [help: true], _, _ } ->
         :help
       { _, [user, project, count], _ } ->
-        { user, project, count }
+        { user, project, String.to_integer(count) }
       { _, [user, project], _ } ->
         { user, project, @default_count }
       _ ->
         :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+
+    System.halt(0)
+  end
+
+  def process({ user, project, _count }) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
