21488af611fa9aa1a71a033b16ac9acc968169cf
Ricardo <bigardone@gmail.com>
2015-11-24 12:53:41 +0100

    Issues module

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,12 @@
+defmodule Issues.GithubIssues do
+  @user_agent [{ "User-agent", "Elixir bigardone@gmail.com" }]
+
+  def fetch(user, project) do
+    issues_url(user, project)
+      |> HTTPoison.get(@user_agent)
+      |> handle_response
+  end
+
+  def handle_response(%{ status_code: 200, body: body }), do: { :ok, body }
+  def handle_response(%{ status_code: ___, body: body }), do: { :error, body }
+end
