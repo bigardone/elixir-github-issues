db9a7f1272e6ea8b57bc49fed402b8b9070b2ff4
Ricardo <bigardone@gmail.com>
2015-11-25 15:36:45 +0100

    Added project variable with github base url

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,5 +1,6 @@
 defmodule Issues.GithubIssues do
   @user_agent [{ "User-agent", "Elixir bigardone@gmail.com" }]
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issues_url(user, project)
@@ -7,6 +8,10 @@ defmodule Issues.GithubIssues do
       |> handle_response
   end
 
-  def handle_response(%{ status_code: 200, body: body }), do: { :ok, body }
-  def handle_response(%{ status_code: ___, body: body }), do: { :error, body }
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response(%{ status_code: 200, body: body }), do: { :ok, :jsx.decode(body) }
+  def handle_response(%{ status_code: ___, body: body }), do: { :error, :jsx.decode(body) }
 end
